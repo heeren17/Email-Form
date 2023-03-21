177dfb3c48ed7c9ff545c4d1ca62bc27de808919
rolan <qyarafisa@gmail.com>
2023-03-21 10:34:37 +0700

    E-mail Form

---
+++ app/src/main/java/com/fahmidwipamane_202102364/emailform/MainActivity.java
@@ -0,0 +1,14 @@
+package com.fahmidwipamane_202102364.emailform;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
